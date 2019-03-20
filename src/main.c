465befcb6f132c72d8cbc93000da3070e9b6201d
uenoa <andre.ueno@technicolor.com>
2019-03-20 15:18:18 -0300

    updated test to demonstrate new features

--- src/main.c
+++ src/main.c
@@ -3,9 +3,104 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include "slist.h"
 
 SLIST_DECLARE(test_list);
+SLIST_DECLARE(test_list2);
+
+struct poster {
+		uint8_t name[30];
+} poster;
+
+void print_func(uint8_t *name) {
+	printf("name found = %s\n", name);
+}
+
+int isPosterName(const void *data, void *ctx) {
+	poster_data *poster_data = ctx;
+	const uint8_t *name = data;
+
+	printf("name found = %s, requested_name  %s\n", name, poster_data->name);
+
+	return (strcmp(name, poster_data->name) == 0);
+}
+
+void addItem(uint8_t *poster_name)
+{
+	slist_t *slot = malloc(sizeof(slist_t));
+	struct poster *data = calloc(1, sizeof(struct poster));
+	strcpy(data->name, poster_name);
+
+	printf("poster to add: %s\n", data->name);
+
+	if(slist_insert(&test_list2, slot, data) != 0 ) {
+		printf("ERROR insert\n");
+	}
+}
+
+void getData(uint8_t *poster_name)
+{
+	uint8_t *name;
+	slist_t *slot;
+
+	name = slist_get_data_when_true(&poster_list,
+								poster_name,
+								isPosterName);
+
+	slot = slist_get_slot_when_true(&test_list2,
+								poster_name,
+								isPosterName,
+								true);
+
+	if(name != NULL) {
+		printf("Found!!! = %s\n", name);
+		free(name);
+		free(slot);
+	} else {
+		printf("could not find poster %s\n", name);
+	}
+
+}
+
+void mytest()
+{
+	addItem("POSTER1");
+	addItem("POSTER2");
+	addItem("POSTER3");
+	addItem("POSTER4");
+	addItem("POSTER5");
+
+	printf("Resulting list:\n");
+	print_list(&test_list2, print_func);
+
+	getData("POSTER1");
+	printf("Resulting list:\n");
+	print_list(&test_list2, print_func);
+	printf("\n\n");
+
+	getData("POSTER1");
+	printf("Resulting list:\n");
+	print_list(&test_list2, print_func);
+	printf("\n\n");
+
+	getData("POSTER1");
+	printf("Resulting list:\n");
+	print_list(&test_list2, print_func);
+	printf("\n\n");
+
+	getData("POSTER1");
+	printf("Resulting list:\n");
+	print_list(&test_list2, print_func);
+	printf("\n\n");
+
+	getData("POSTER1");
+	printf("Resulting list:\n");
+	print_list(&test_list2, print_func);
+	printf("\n\n");
+
+}
 
 
 /**
@@ -13,17 +108,5 @@ SLIST_DECLARE(test_list);
  */
 int main (int argc, char **argv)
 {
-	for (int i = 1; i < 64; i ++) {
-		slist_t *slot = malloc(sizeof(slist_t));
-		int *data = malloc(sizeof(int));
-		*data = i;
-		if(slist_insert(&test_list, slot, data) == 0 ) {
-			int int_data = 0;
-			slist_get_data_from_tail(&test_list, &int_data, false);
-			printf("current tail of list is: %d \n\r", int_data);
-		}else {
-			printf("error inserting data on list \n\r");
-		}
-	}
-	return 0;
+	mytest();
 }
